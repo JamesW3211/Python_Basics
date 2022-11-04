8cf7306c73093a19b240d493de4ed69e640a18aa
JamesW3211 <108100056+JamesW3211@users.noreply.github.com>
2022-11-04 10:16:54 -0400

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,35 @@
+"""
+Created by JamesW
+This file was created with the purpose of learning the basics of Python programing
+"""
+
+
+
+"""
+You can change the value of a variable in your program at any time,and Python will always keep track of its current value
+"""
+
+message = "Hello World" #The value hello world was assigned to the variable message.
+print(message)          #The value assigned to the variable message was then displayed on screen.
+
+message = "3e3"  #The same variable message can be assigned a new value. The last value assigned to the variable is the one the variable contains.
+print(message)   #The new value is displayed on screen.
+
+
+
+"""
+RULES TO REMEMBER 
+•	 Variable names can contain only letters, numbers, and underscores.
+They can start with a letter or an underscore, but not with a number.
+For instance, you can call a variable message_1 but not 1_message.
+•	 Spaces are not allowed in variable names, but underscores can be used
+to separate words in variable names. For example, greeting_message works,
+but greeting message will cause errors.
+•	 Avoid using Python keywords and function names as variable names;
+that is, do not use words that Python has reserved for a particular programmatic purpose, such as the word print. 
+•	 Variable names should be short but descriptive. 
+•	 Be careful when using the lowercase letter l and the uppercase letter O
+because they could be confused with the numbers 1 and 0.
+"""
+
+
