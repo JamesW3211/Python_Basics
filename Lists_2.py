0fd2efa6e0f96c953c1695b6f3999026f0898fad
JamesW3211 <108100056+JamesW3211@users.noreply.github.com>
2023-06-10 15:10:43 -0400

    Update Lists_2.py

--- Lists_2.py
+++ Lists_2.py
@@ -82,3 +82,13 @@ print(squares)
 
 
 """Simple Statistics with a List of Numbers"""
+
+"""A few Python functions are specific to lists of numbers. For example, you
+can easily find the minimum, maximum, and sum of a list of numbers:"""
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+
